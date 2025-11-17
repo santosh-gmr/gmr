0c8b4ab3e1a914622a10a8a0718227facb243163
Santosh <cnlt8079@gmrgroup.in>
2025-11-17 19:09:27 +0530

    updated component-model.json

--- blocks/footer-topbar/footer-topbar.js
+++ blocks/footer-topbar/footer-topbar.js
@@ -3,12 +3,12 @@ export default function decorate(block) {
 
   const logo = data.logo || "";
   const social = data.social || [];
-  const buttonLabel = data.buttonLabel || "Group Websites";
-  const buttonIcon = data.buttonIcon || "+";
+  const button = data.button || "Group Websites";
+  const text = data.text || "";
 
   block.innerHTML = `
     <div class="footer-topbar">
-      
+
       <div class="footer-topbar-left">
         ${logo ? `<img src="${logo}" alt="logo" class="footer-logo">` : ""}
       </div>
@@ -16,19 +16,16 @@ export default function decorate(block) {
       <div class="footer-topbar-right">
 
         <div class="footer-social-icons">
-          ${social
-            .map(
-              (s) => `
-              <a href="${s.url}" class="footer-social-icon" target="_blank">
-                <span>${s.icon}</span>
-              </a>`
-            )
-            .join("")}
+          ${social.map(item => `
+            <a href="${item.url}" target="_blank" class="footer-social-link">
+              <img src="${item.icon}" class="footer-social-icon" />
+            </a>
+          `).join("")}
         </div>
 
         <button class="footer-topbar-btn">
-          ${buttonLabel}
-          <span class="footer-topbar-btn-icon">${buttonIcon}</span>
+          ${button}
+          <span class="footer-btn-icon">+</span>
         </button>
 
       </div>
