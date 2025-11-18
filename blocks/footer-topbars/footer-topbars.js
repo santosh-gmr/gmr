546ec2dfd4ef672644eee874ebe6033bcd829885
Santosh <cnlt8079@gmrgroup.in>
2025-11-18 10:37:43 +0530

    footer

---
+++ blocks/footer-topbars/footer-topbars.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+  // CREATE a container before adding the row structure
+
+
+
+  rows.forEach((row) => {
+     const cells = [...row.children];
+     console.log(cells[0]);
+  });
