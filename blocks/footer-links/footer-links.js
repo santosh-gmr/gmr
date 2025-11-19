e699f88a907e67c5a63bbcfbe59363f59a67d1b9
santosh-gmr <cnlt8079@gmrgroup.in>
2025-11-19 15:30:54 +0530

    Update footer-links.js

--- blocks/footer-links/footer-links.js
+++ blocks/footer-links/footer-links.js
@@ -36,8 +36,12 @@ export default function decorate(block) {
 
     // Build YOUR custom structure
     const col = document.createElement("div");
-    col.classList.add("col", "w-20", safeClass);
-    //col.classList.add("col", "w-20");
+    if(value!='' && value!='undefined')
+    {
+      col.classList.add("col", "w-20", safeClass);
+    }else {
+      col.classList.add("col", "w-20");
+    }
 
     const titleWrapper = document.createElement("div");
     titleWrapper.classList.add("footer-nav");
