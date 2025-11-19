0155a3a89e9a6dddeb6f5effc3b2a0236422f127
santosh-gmr <cnlt8079@gmrgroup.in>
2025-11-19 17:39:41 +0530

    Update footer-links.js

--- blocks/footer-links/footer-links.js
+++ blocks/footer-links/footer-links.js
@@ -18,45 +18,45 @@ export default function decorate(block) {
   // Get each original row inside container
   const rows = [...container.children];
 
-  rows.forEach((row) => {
-    const cells = [...row.children];
+  // rows.forEach((row) => {
+  //   const cells = [...row.children];
 
-    if (cells.length < 2) return;
+  //   if (cells.length < 2) return;
 
-    const titleDiv = cells[0];
-    const contentDiv = cells[1];
+  //   const titleDiv = cells[0];
+  //   const contentDiv = cells[1];
 
-    // Read heading for classname
-    const value = contentDiv.textContent.trim();
-    //const value = titleDiv.textContent.trim();
+  //   // Read heading for classname
+  //   const value = contentDiv.textContent.trim();
+  //   //const value = titleDiv.textContent.trim();
 
-    // Build YOUR custom structure
-    const col = document.createElement("div");
-    if(value!='' && value!='undefined')
-    {
-      const safeClass = value.toLowerCase().replace(/\s+/g, "-");
-      col.classList.add("col", "w-20", safeClass);
-    }else {
-      col.classList.add("col", "w-20");
-    }
+  //   // Build YOUR custom structure
+  //   const col = document.createElement("div");
+  //   if(value!='' && value!='undefined')
+  //   {
+  //     const safeClass = value.toLowerCase().replace(/\s+/g, "-");
+  //     col.classList.add("col", "w-20", safeClass);
+  //   }else {
+  //     col.classList.add("col", "w-20");
+  //   }
 
-    const titleWrapper = document.createElement("div");
-    titleWrapper.classList.add("footer-nav");
+  //   const titleWrapper = document.createElement("div");
+  //   titleWrapper.classList.add("footer-nav");
 
-    const contentWrapper = document.createElement("div");
-    contentWrapper.classList.add("footer-text");
+  //   const contentWrapper = document.createElement("div");
+  //   contentWrapper.classList.add("footer-text");
 
-    // Move editable nodes (UE keeps edit capability)
-    titleWrapper.append(...titleDiv.childNodes);
-    //contentWrapper.append(...contentDiv.childNodes);
+  //   // Move editable nodes (UE keeps edit capability)
+  //   titleWrapper.append(...titleDiv.childNodes);
+  //   //contentWrapper.append(...contentDiv.childNodes);
 
-    // Final structure
-    //col.append(titleWrapper, contentWrapper);
-    col.append(titleWrapper);
+  //   // Final structure
+  //   //col.append(titleWrapper, contentWrapper);
+  //   col.append(titleWrapper);
 
-    // Replace original table row with new column markup
-    row.replaceWith(col);
-  });
+  //   // Replace original table row with new column markup
+  //   row.replaceWith(col);
+  // });
 }
 
 
