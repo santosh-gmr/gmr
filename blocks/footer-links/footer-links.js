175d40bb385f30439d4c6fc8de220936cc536dec
santosh-gmr <cnlt8079@gmrgroup.in>
2025-11-19 15:32:23 +0530

    Update footer-links.js

--- blocks/footer-links/footer-links.js
+++ blocks/footer-links/footer-links.js
@@ -29,15 +29,14 @@ export default function decorate(block) {
     // Read heading for classname
     const value = contentDiv.textContent.trim();
     //const value = titleDiv.textContent.trim();
-     console.log('value=');
-    console.log(value);
-    const safeClass = value.toLowerCase().replace(/\s+/g, "-");
-    //const safeClass = '';
 
     // Build YOUR custom structure
     const col = document.createElement("div");
     if(value!='' && value!='undefined')
     {
+        console.log('value=');
+        console.log(value);
+      const safeClass = value.toLowerCase().replace(/\s+/g, "-");
       col.classList.add("col", "w-20", safeClass);
     }else {
       col.classList.add("col", "w-20");
