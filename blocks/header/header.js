2ec110451187f52cd45e74fdcafd45943b241eb9
santosh-gmr <cnlt8079@gmrgroup.in>
2025-11-28 17:00:39 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -110,7 +110,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/en/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
@@ -164,3 +164,4 @@ export default async function decorate(block) {
   navWrapper.append(nav);
   block.append(navWrapper);
 }
+
