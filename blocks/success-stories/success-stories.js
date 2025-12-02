cf9ad7e5e13f267f953af45076889dbd2004c8f2
santosh-gmr <cnlt8079@gmrgroup.in>
2025-12-02 15:22:28 +0530

    Update success-stories.js

--- blocks/success-stories/success-stories.js
+++ blocks/success-stories/success-stories.js
@@ -1,5 +1,5 @@
 export default async function decorate(block) {
-  const resp = await fetch('https://author-p168597-e1803019.adobeaemcloud.com/graphql/execute.json/GMR/news-list', {
+  const resp = await fetch('/graphql/execute.json/GMR/news-list', {
     headers: { Accept: 'application/json' },
   });
 
@@ -11,6 +11,7 @@ export default async function decorate(block) {
 
   const json = await resp.json();
 }
+
 
 
 
