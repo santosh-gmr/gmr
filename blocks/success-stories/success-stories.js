e659310f6db28227d52146aff13643cd71776faa
santosh-gmr <cnlt8079@gmrgroup.in>
2025-12-03 13:37:42 +0530

    Update success-stories.js

--- blocks/success-stories/success-stories.js
+++ blocks/success-stories/success-stories.js
@@ -1,13 +1,1 @@
-export default async function decorate(block) {
 
-  const endpoint = '/graphql/execute.json?persistedQuery=GMR/news-list';
-
-  const resp = await fetch(endpoint, {
-    method: 'GET',
-    headers: { Accept: 'application/json' }
-  });
-
-  console.log(resp);
-  const json = await resp.json();
-  console.log("DATA:", json);
-}
